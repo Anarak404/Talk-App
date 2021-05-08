ad234a75cace9322681c2b0877c76c5dbd9012bc
Anarak404 <annaarak2@gmail.com>
2021-05-08 14:17:09 +0200

    feat(model): user entity

---
+++ server/src/main/java/pl/talkapp/server/model/User.java
@@ -0,0 +1,48 @@
+package pl.talkapp.server.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "app_user")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NonNull
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    @NonNull
+    private String email;
+
+    @Column(nullable = false)
+    @NonNull
+    private String password;
+
+    @NonNull
+    private String photo;
+
+    @CreationTimestamp
+    @NonNull
+    private LocalDateTime creationDateTime;
+
+}
+

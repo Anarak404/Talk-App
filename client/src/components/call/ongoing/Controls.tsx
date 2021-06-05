198fa6056342f4dcb4c107d16a06bf1c84daa44a
siekiera-a <siekiera.a31@gmail.com>
2021-06-05 16:08:38 +0200

    fix(components): endCall on press

--- client/src/components/call/ongoing/Controls.tsx
+++ client/src/components/call/ongoing/Controls.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { FAB, Icon } from 'react-native-elements';
+import { callContext } from '../../../context/CallContext';
 
 export function Controls() {
+  const { endCall } = useContext(callContext);
+
   return (
     <View style={styles.container}>
       <FAB
         buttonStyle={[styles.buttonStyle, styles.disconnectButton]}
         icon={<Icon name="call-end" size={iconSize} />}
         containerStyle={styles.buttonContainer}
+        onPress={endCall}
       />
     </View>
   );

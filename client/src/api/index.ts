ab3df40d28c17a68326d93ed280d822e2d4102c2
siekiera-a <siekiera.a31@gmail.com>
2021-06-06 17:41:42 +0200

    feat(api): start call

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,5 +1,6 @@
-export { IAuthenticationResponse } from './user/types';
+export { startCall } from './call/start';
+export { ICallRequest, ICallResponse } from './call/types';
 export { ErrorResponse, IError } from './error';
-
-export { register, IRegisterRequest } from './user/register';
-export { login, ILoginRequest } from './user/login';
+export { ILoginRequest, login } from './user/login';
+export { IRegisterRequest, register } from './user/register';
+export { IAuthenticationResponse } from './user/types';

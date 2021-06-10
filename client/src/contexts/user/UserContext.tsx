4f1578984b8f714d97fc9c07714cf2b05a6cc15a
siekiera-a <siekiera.a31@gmail.com>
2021-06-10 20:53:59 +0200

    fix(context): user status removed

--- client/src/contexts/user/UserContext.tsx
+++ client/src/contexts/user/UserContext.tsx
@@ -1,9 +1,8 @@
 import React, { createContext, useCallback, useContext, useState } from 'react';
 import { callContext } from '../call/CallContext';
-import { IUserContext, IUserContextProps, Status } from './UserTypes';
+import { IUserContext, IUserContextProps } from './UserTypes';
 
 const defaultValue: IUserContext = {
-  status: Status.OFFLINE,
   startCall: () => void 0,
 };
 
@@ -13,12 +12,11 @@ const { Provider } = userContext;
 
 export function UserContextProvider({ userId, children }: IUserContextProps) {
   const [id] = useState(userId);
-  const [status, setStatus] = useState(Status.OFFLINE);
   const { startCall } = useContext(callContext);
 
   const start = useCallback(() => {
     startCall(id);
   }, [id, startCall]);
 
-  return <Provider value={{ status, startCall: start }}>{children}</Provider>;
+  return <Provider value={{ startCall: start }}>{children}</Provider>;
 }
--- client/src/contexts/user/UserTypes.ts
+++ client/src/contexts/user/UserTypes.ts
@@ -1,17 +1,10 @@
 import React from 'react';
 
 export interface IUserContext {
-  status: Status;
   startCall(): void;
 }
 
 export interface IUserContextProps {
   userId: number;
   children?: React.ReactNode;
-}
-
-export enum Status {
-  ONLINE,
-  OFFLINE,
-  BUSY,
 }

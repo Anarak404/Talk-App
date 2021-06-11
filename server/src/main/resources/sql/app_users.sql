c66db88c3cccbfc9c405850d07bce81adb3a25fc
Anarak404 <annaarak2@gmail.com>
2021-06-12 00:35:13 +0200

    feat(data): initial data

---
+++ server/src/main/resources/sql/app_users.sql
@@ -0,0 +1,13 @@
+-- login: email, password: Ania123!
+insert into app_user (creation_date_time, email, name, password, photo)
+values (now(), 'ania@op.pl', 'Ania', '$2y$10$GH1ha1kr8wTEXICd24Z.Ku85wd2rzaUQHjDkIV8HqmVm9ALtNbKAy', null),
+       (now(), 'ola@op.pl', 'Ola', '$2y$10$JAFUreeGn/ecgMX7xPlRPeKAlaEbCTisCzEtqgJT5bPxPKqw7OScu', 'https://www.colorland.pl/sites/default/files/article-image/kot1_1.jpg'),
+       (now(), 'ala@op.pl', 'Ala', '$2y$10$hWQdnU0KP1kdpApuA7OtsuEjxA817u3iOuOYnqDg4gq3u605pd9Qe', 'https://www.odkrywamyzakryte.com/wp-content/uploads/2017/05/56999502_l.jpg'),
+       (now(), 'tomek@op.pl', 'Tomek', '$2y$10$ZHCRzNP.YqQ8BGekmHSEq.xuWZrtPVC0dUoWwo13ph/0/Q0ooAk72', 'https://czasnamarketing.pl/wp-content/uploads/2019/02/key1-1024x623.jpg'),
+       (now(), 'arek@op.pl', 'Arek', '$2y$10$D.tqzy0fvMwUTVeKEVdBcOU3Pzr26cRnGpEwqhQu.bbw0FRkZad9m', 'https://www.mypolacy.de/files/avatars/5d07af463c57b841310441a7_fb-1560784710981.jpg'),
+       (now(), 'kamil@op.pl', 'Kamil', '$2y$10$y1NwrKTPyqUFgt7WdUvDMuVi6.AoevUvs9VpSfghs41KBad6VbJZS', 'https://www.mypolacy.de/files/avatars/5dd51b7a9444ec3974489816_fb-1574247291388.jpg'),
+       (now(), 'magda@op.pl', 'Magda', '$2y$10$pOg7jcCb5DXyjVRZgK/7w.uZWg.NHEqhLm.sQaF4GjRWX.3f5Z4Aq', 'https://www.foto4u.pl/blog/artykul/!uploads/profilowe/profesjonalne.jpg'),
+       (now(), 'piotr@op.pl', 'Piotr', '$2y$10$Aj3ihtdMJp8Zl7kDkqWITehEdoNgJ.kw51KDRto1LqwTbVeSvHUE.', 'https://lh3.googleusercontent.com/proxy/nnRJ2-EecMAebNzhTH1jkAFHT5LYoZF-1d3-7BVTNV3zUhAajvYYmC1gC6Gubo6Fqd8xy_9iTlXSnk_runxprPpcP9InyR1Avow7qyv1UWxMWyXPCnxsVvL-HnpgjomfscxoOsSSV3SW9jUNMsGZBRoW5snbiYCK9Fmg0TE06qdasLpXfwusHh8MWTqofJyWjuVENAGqEP4XaeIB5SfP'),
+       (now(), 'marta@op.pl', 'Marta', '$2y$10$icA1uwRcPc43R6rsj6G.OeZiBwv49u5cSGj9cb1VEwvN2Y65tK8ja', 'https://i0.wp.com/karieranaobcasach.com/wp-content/uploads/2020/04/Zdjecie-profilowe.png?fit=560%2C315&ssl=1'),
+       (now(), 'adrian@op.pl', 'Adrian', '$2y$10$IJNIGL4HY3PWrnIquGoEwOHJJ/MFbpWxe7MV7XPD.Ntfm5LIIYOcC', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX512qexq3hickX7mPlf2OvC16YPjXtjDDnA&usqp=CAU'),
+       (now(), 'bogdan@op.pl', 'Bogdan', '$2y$10$hU4qxykFflulaTMLYPaEHuturEkyBOU1FP05kpC6X4CnW0NGR/C56', 'https://i.redd.it/a9q2jze2m0a41.jpg');
---
+++ server/src/main/resources/sql/servers.sql
@@ -0,0 +1,8 @@
+insert into server (creation_date_time, name, photo, owner)
+values (now(), 'Thor', 'https://bi.im-g.pl/im/9c/7b/15/z22526876V,Kadr-z-filmu--Thor--Ragnarok-.jpg', 1),
+       (now(), 'Oslo', 'https://desenio.pl/bilder/artiklar/zoom/7837_2.jpg?imgwidth=435&qt=', 3),
+       (now(), 'Apollo', 'https://ciekawamitologia.pl/images/apollo.jpg', 4),
+       (now(), 'Super Mario', 'https://ocdn.eu/images/pulscms/MzQ7MDA_/1d4c2a6858e2bdebcad926d9a51f69e2.jpg', 6),
+       (now(), 'Onopedia', 'https://static.wikia.nocookie.net/onopedia/images/a/a5/Kaczy%C5%84ski.jpg/revision/latest/smart/width/250/height/250?cb=20171016081303&path-prefix=pl', 7),
+       (now(), 'Slugterra', 'https://cdn.hbogo.eu/images/A7949242-BA62-4FA7-94DD-EAEE30A169A7/1280_720.jpg', 10),
+       (now(), 'League of Legends', 'https://lenovogaming.pl/files/Gry/2021/Free/3.jpg', 11);
